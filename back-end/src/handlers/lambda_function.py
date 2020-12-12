7012a7f8fe43c981931bfb187fd53e167d82b97d
pujaredij17 <pujaredij17@gmail.com>
2020-12-11 21:01:11 -0600

    update 3

--- back-end/src/handlers/lambda_function.py
+++ back-end/src/handlers/lambda_function.py
@@ -28,7 +28,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "isBase64Encoded": False,
         "headers": {
-            "Access-Control-Allow-Origin": "http://onlineresumeprofile.s3-website-us-east-1.amazonaws.com"
+            "Access-Control-Allow-Origin": "http://dulpchs9918zr.cloudfront.net/index.html"
         },
         "body": json.dumps(item['Item']['counter_value'], indent=4, cls=DecimalEncoder)
     }
